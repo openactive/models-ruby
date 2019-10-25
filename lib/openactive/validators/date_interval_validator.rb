0a281c39c23c9bf2e325faa741cae5910351ec86
Joe Carter <joe@simpleweb.co.uk>
2019-10-25 10:46:12 +0100

    Date interval validation

--- lib/openactive/validators/date_interval_validator.rb
+++ lib/openactive/validators/date_interval_validator.rb
@@ -1,65 +1,45 @@
 module OpenActive
-module Validators
-
-use OpenActive::Helpers::DateInterval as DateIntervalHelper
-
-class DateIntervalValidator < BaseValidator
-{
-    
-     # Coerce given value to the type the validator is validating against.
-     # PLEASE NOTE: no checks are performed on the given $value.
-     # It is therefore recommended to call the "run" method first before this.
-     #
-     # @param mixed $value The value to coerce.
-     # @return mixed The same value.
-     # @throws ::Exception When $value cannot be parsed as a ::DateInterval
-     
-    def coerce($value)
-    {
+  module Validators
+    class DateIntervalValidator < BaseValidator
+      # Coerce given value to the type the validator is validating against.
+      # PLEASE NOTE: no checks are performed on the given $value.
+      # It is therefore recommended to call the "run" method first before this.
+      #
+      # @param mixed $value The value to coerce.
+      # @return mixed The same value.
+      # @throws ::Exception When $value cannot be parsed as a ::DateInterval
+      def coerce(value)
         # instanceof does not throw any error
         # if the variable being tested is not an object,
         # it simply returns FALSE.
-        if ($value instanceof ::DateInterval) {
-            return $value
-        }
+        return value if value.is_a?(ActiveSupport::Duration)
 
         # If not passing a DateInterval object, try and create one from $value
         # Assuming that the string will be an interval spec in ISO 8601 format
-        return new ::DateInterval($value)
-    }
+        ActiveSupport::Duration.parse(value)
+      end
 
-    
-     # Run validation on the given value.
-     #
-     # @param mixed $value The value to validate.
-     # @return bool Whether validation passes or not.
-     
-    def run($value)
-    {
+      # Run validation on the given value.
+      #
+      # @param mixed $value The value to validate.
+      # @return bool Whether validation passes or not.
+
+      def run(value)
         # instanceof does not throw any error
         # if the variable being tested is not an object,
         # it simply returns FALSE.
-        if ($value instanceof ::DateInterval) {
-            return true
-        }
+        return true if value.is_a?(ActiveSupport::Duration)
 
         # If not a string - fail validation
-        if (is_string($value) === false) {
-            return false
-        }
+        return false unless value.is_a?(String)
 
-        # If not passing a DateInterval object, try and create one from $value
-        try {
-            $dateInterval = new ::DateInterval($value)
-        } catch (::Exception $e) {
-            # If the string is not a valid interval spec in ISO 8601 format,
-            # Fail validation
-            return false
-        }
-
-        # Double check that the created interval returns the same interval spec
-        # As $value if so, validation passes
-        return $dateInterval !== false &&
-            DateIntervalHelper::specString($dateInterval) === $value
-    }
-}
+        begin
+          ActiveSupport::Duration.parse(value)
+          true
+        rescue ArgumentError => _e
+          false
+        end
+      end
+    end
+  end
+end
--- lib/openactive/validators/float_validator.rb
+++ lib/openactive/validators/float_validator.rb
@@ -1,29 +1,27 @@
 module OpenActive
-module Validators
+  module Validators
+    class FloatValidator < BaseValidator
+      # Coerce given value to the type the validator is validating against.
+      # PLEASE NOTE: no checks are performed on the given $value.
+      # It is therefore recommended to call the "run" method first before this.
+      #
+      # @param mixed $value The value to coerce.
+      # @return float The coerced value
+      def coerce(value)
+        value.to_f
+      end
 
-class FloatValidator < BaseValidator
-{
-    
-     # Coerce given value to the type the validator is validating against.
-     # PLEASE NOTE: no checks are performed on the given $value.
-     # It is therefore recommended to call the "run" method first before this.
-     #
-     # @param mixed $value The value to coerce.
-     # @return float The coerced value
-     
-    def coerce($value)
-    {
-        return (float) $value
-    }
+      # Run validation on the given value.
+      #
+      # @param mixed $value The value to validate.
+      # @return bool Whether validation passes or not.
 
-    
-     # Run validation on the given value.
-     #
-     # @param mixed $value The value to validate.
-     # @return bool Whether validation passes or not.
-     
-    def run($value)
-    {
-        return is_int($value) || is_float($value)
-    }
-}
+      def run(value)
+        Float(value)
+        true
+      rescue ArgumentError => _e
+        false
+      end
+    end
+  end
+end
