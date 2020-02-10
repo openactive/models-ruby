932679e6556847d4e4bb88cd205613a6d4d75ccb
Patrick Sinclair <metade@gmail.com>
2020-02-10 19:17:43 +0000

    Remove @ from RpdeItem id field
    When validating an RPDE feed generated using this gem we got validation
    errors caused by `@` being prepended to the RPDE item id field.
    
    According to Nick Evans' message on the OpenActive Slack:
    > this is because RPDE is not JSON-LD based - only everything within
    > the RPDE property "data", where the "@context" is defined, is JSON-LD
    > and hence uses @id
    
    Therefore changing OpenActive::Rpde::RpdeItem#id property to be `id`
    instead of `@id`.

--- lib/openactive/rpde/rpde_item.rb
+++ lib/openactive/rpde/rpde_item.rb
@@ -3,7 +3,7 @@ module OpenActive
     class RpdeItem < OpenActive::BaseModel
       define_property :state, types: ["OpenActive::Rpde::RpdeState"]
       define_property :kind, types: ["OpenActive::Rpde::RpdeKind"]
-      define_property :id, types: ["string", "int"], as: "@id"
+      define_property :id, types: ["string", "int"], as: "id"
       define_property :modified, types: ["int"]
       define_property :data, types: ["OpenActive::BaseModel", "null"]
     end
